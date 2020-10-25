70164b3613844d6564656ec8f12a0716bcc92fd5
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-10-25 19:03:20 +0530

    Add files via upload

---
+++ Python/Lists.py
@@ -0,0 +1,19 @@
+T = int(input().strip())
+
+L = []
+for t in range(T):
+    args = input().strip().split(" ")
+    if args[0] == "append":
+        L.append(int(args[1]))
+    elif args[0] == "insert":
+        L.insert(int(args[1]), int(args[2]))
+    elif args[0] == "remove":
+        L.remove(int(args[1]))
+    elif args[0] == "pop":
+        L.pop()
+    elif args[0] == "sort":
+        L.sort()
+    elif args[0] == "reverse":
+        L.reverse()
+    elif args[0] == "print":
+        print(L)
