679586790bff0e4aee8628b2342958da13736c33
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-10-26 21:18:35 +0530

    Add files via upload

---
+++ Python/Nested list.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    a= []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        a.append([score, name])
+
+    a.sort()
+    b = [i for i in a if i[0] != a[0][0]]
+    c = [j for j in b if j[0] == b[0][0]]
+    
+    c.sort(key=lambda x: x[1])
+    for i in range(len(c)):
+        print(c[i][1])
