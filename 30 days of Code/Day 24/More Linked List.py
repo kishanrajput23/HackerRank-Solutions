781b48d479203f995d6b75f698b746156a04041b
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-08 19:44:16 +0530

    Add files via upload

---
+++ 30 days of Code/Day 24/More Linked List.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def insert(self,head,data):
+            p = Node(data)           
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head  
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        #Write your code here
+        c = head
+        while True:
+            try:
+                if c.data == c.next.data:
+                   c.next =  c.next.next
+                else:  c  =  c.next
+            except AttributeError: # final None has no data
+                return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+head=mylist.removeDuplicates(head)
+mylist.display(head); 
