a73fa636102f912b8c6682551964eb2fc609536b
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-14 13:31:44 +0530

    Add files via upload

---
+++ C++/Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout << a << "\n" << b << "\n" << c << "\n";
+    printf("%.3f\n", d); 
+    printf("%.9lf", e);
+    return 0;
+}
