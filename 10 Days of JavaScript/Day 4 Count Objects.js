e3f78d55d0609c0138250dae1fdd8f096f18d167
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-05-24 18:50:26 +0530

    Count Objects

---
+++ 10 Days of JavaScript/Day 4 Count Objects.js
@@ -0,0 +1,38 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    let n = 0;
+    for(let o of objects) {
+        n += (o.x == o.y);
+    }
+    return n;
+}
+
