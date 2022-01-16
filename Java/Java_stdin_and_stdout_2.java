654d9e786afd6d423604275334cddfa53a1bb12e
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-01-16 17:41:04 +0530

    Java Solutions

---
+++ Java/Java_stdin_and_stdout_2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+
+        // Write your code here.
+        Double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        scan.close();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
