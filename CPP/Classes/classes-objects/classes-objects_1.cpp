aca33d4d6ec1cf0d43445a704afc6353e9d5316d
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-08-10 22:16:16 +0530

    Added Classes and Objects solution - CodeHub Sync

---
+++ CPP/Classes/classes-objects/classes-objects_1.cpp
@@ -0,0 +1,23 @@
+
+
+// Write your Student class here
+class Student {
+    private:
+    int scores[5];
+    
+    public:
+    void input() {
+        for (int i = 0; i < 5; i++) {
+            cin >> scores[i];
+        }
+    }
+    
+    int calculateTotalScore() {
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += scores[i];
+        }
+        return sum;
+    }
+};
+
