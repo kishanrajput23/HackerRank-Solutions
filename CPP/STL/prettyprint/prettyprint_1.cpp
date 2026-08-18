16833d46baf95fd89ea9d04d7cac60c22840e92c
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-08-18 22:02:22 +0530

    Added Print Pretty solution - CodeHub Sync

---
+++ CPP/STL/prettyprint/prettyprint_1.cpp
@@ -0,0 +1,13 @@
+
+
+		/* Enter your code here */
+        cout << hex << setw(0) << setiosflags(ios::showbase) << resetiosflags(ios::uppercase);
+        cout << (long long)A << endl;
+
+        cout << right << setfill('_') << setw(15) << setprecision(2) << fixed << setiosflags(ios::showpos);
+        cout << B << endl;
+
+        cout << resetiosflags(ios::showpos) << setiosflags(ios::uppercase) << setprecision(9) << scientific;
+        cout << C << endl;
+        
+
