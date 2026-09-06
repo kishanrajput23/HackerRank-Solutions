923820099775ed350d98603fb4967bbb1b6eb7cc
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-09-06 13:23:53 +0530

    Added CamelCase solution - CodeHub Sync

---
+++ Algorithms/Strings/camelcase/camelcase_1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int camelcase(string s) {
+    int count = 1;
+    for (auto i : s) {
+        if (isupper(i)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
