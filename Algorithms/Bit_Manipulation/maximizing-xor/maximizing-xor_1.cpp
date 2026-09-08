6b14431191eb47d31c91259783f4ac94b85664fc
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-09-08 22:01:27 +0530

    Added Maximizing XOR solution - CodeHub Sync

---
+++ Algorithms/Bit_Manipulation/maximizing-xor/maximizing-xor_1.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'maximizingXor' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER l
+ *  2. INTEGER r
+ */
+
+int maximizingXor(int l, int r) {
+    int maxi = INT_MIN;
+    
+    for (int i=l; i<=r; i++) {
+        for (int j=l; j<=r; j++) {
+            maxi = max(maxi, i^j);
+        }
+    }
+    
+    return maxi;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string l_temp;
+    getline(cin, l_temp);
+
+    int l = stoi(ltrim(rtrim(l_temp)));
+
+    string r_temp;
+    getline(cin, r_temp);
+
+    int r = stoi(ltrim(rtrim(r_temp)));
+
+    int result = maximizingXor(l, r);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
