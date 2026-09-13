241608bc35f53450755fae800a452d54e2330430
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-09-13 10:09:56 +0530

    Added Append and Delete solution - CodeHub Sync

---
+++ Algorithms/Implementation/append-and-delete/append-and-delete_3.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'appendAndDelete' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. STRING t
+ *  3. INTEGER k
+ */
+
+string appendAndDelete(string s, string t, int k) {
+    int common = 0;
+
+    while (common < s.size() &&
+           common < t.size() &&
+           s[common] == t[common]) {
+        common++;
+    }
+
+    int operations = (s.size() - common) + (t.size() - common);
+
+    if ((k >= operations && (k - operations) % 2 == 0) ||
+        k >= s.size() + t.size()) {
+        return "Yes";
+    }
+
+    return "No";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string t;
+    getline(cin, t);
+
+    string k_temp;
+    getline(cin, k_temp);
+
+    int k = stoi(ltrim(rtrim(k_temp)));
+
+    string result = appendAndDelete(s, t, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
