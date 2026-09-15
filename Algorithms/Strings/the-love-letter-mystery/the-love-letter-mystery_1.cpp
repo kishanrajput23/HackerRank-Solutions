a6c480778182735952a3a978f0c26503edec4651
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-09-15 18:43:58 +0530

    Added The Love-Letter Mystery solution - CodeHub Sync

---
+++ Algorithms/Strings/the-love-letter-mystery/the-love-letter-mystery_1.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'theLoveLetterMystery' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int theLoveLetterMystery(string s) {
+    int count = 0;
+    int n = s.size();
+    
+    for (int i=0; i<n/2; i++) {
+        if (s[i] == s[n-i-1]) {
+            continue;
+        }
+        else {
+            count += abs(s[n-i-1] - s[i]);
+        }
+    }
+    
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = theLoveLetterMystery(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
