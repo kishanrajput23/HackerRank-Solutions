a20128e942049355d3007b61a0a63135d1d22508
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-09-16 20:45:33 +0530

    Added Gemstones solution - CodeHub Sync

---
+++ Algorithms/Strings/gem-stones/gem-stones_2.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'gemstones' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING_ARRAY arr as parameter.
+ */
+
+int gemstones(vector<string> arr) {
+    set<char> st(arr[0].begin(), arr[0].end());
+
+    for (int i = 1; i < arr.size(); i++) {
+        set<char> temp(arr[i].begin(), arr[i].end());
+
+        for (auto it = st.begin(); it != st.end(); ) {
+            if (!temp.count(*it))
+                it = st.erase(it);
+            else
+                ++it;
+        }
+    }
+
+    return st.size();
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<string> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        string arr_item;
+        getline(cin, arr_item);
+
+        arr[i] = arr_item;
+    }
+
+    int result = gemstones(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
