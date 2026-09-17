43f0837769a98e5b6c42ab622ae5321d950e5d32
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-09-17 20:16:50 +0530

    Added String Construction  solution - CodeHub Sync

---
+++ Algorithms/Strings/string-construction/string-construction_1.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'stringConstruction' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int stringConstruction(string s) {
+    set<char> st;
+    int count = 0;
+    for (auto i : s) {
+        if (!st.count(i)) {
+            count++;
+            st.insert(i);
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = stringConstruction(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
