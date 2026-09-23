2a5e0de044ad43bc9942588649a7db87ab07f7df
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-09-23 20:10:21 +0530

    Added Repeated String solution - CodeHub Sync

---
+++ Algorithms/Implementation/repeated-string/repeated-string_1.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+long repeatedString(string s, long n) {
+    long len = s.size();
+
+    long count_a = 0;
+    for (char c : s) {
+        if (c == 'a')
+            count_a++;
+    }
+
+    long full = n / len;
+    long remaining = n % len;
+
+    long ans = count_a * full;
+
+    for (int i = 0; i < remaining; i++) {
+        if (s[i] == 'a')
+            ans++;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    long n = stol(ltrim(rtrim(n_temp)));
+
+    long result = repeatedString(s, n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
