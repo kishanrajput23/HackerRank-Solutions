9139242b61b98b15c752f3b6b81846a6631595b4
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-09-26 20:05:56 +0530

    Added C++ Variadics solution - CodeHub Sync

---
+++ CPP/Other_Concepts/cpp-variadics/cpp-variadics_1.cpp
@@ -0,0 +1,10 @@
+
+
+// Enter your code for reversed_binary_value<bool...>()
+
+template <bool a> int reversed_binary_value() { return a; }
+
+template <bool a, bool b, bool... d> int reversed_binary_value() {
+  return (reversed_binary_value<b, d...>() << 1) + a;
+}
+
